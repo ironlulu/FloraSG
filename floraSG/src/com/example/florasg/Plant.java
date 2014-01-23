725d7a442740b85d9900b41db7087808105480a4
minwo324 <aprillee2412720@gmail.com>
2014-01-23 16:10:37 +0800

    Modify Plant class

--- floraSG/src/com/example/florasg/Plant.java
+++ floraSG/src/com/example/florasg/Plant.java
@@ -38,6 +38,113 @@ public class Plant {
 		associatedFauna = _associatedFauna;
 		bookmarkStatus = _bookmarkStatus;				
 	}
-	
+
+	public int getSpeciesID() {
+		return speciesID;
+	}
+
+	public void setSpeciesID(int speciesID) {
+		this.speciesID = speciesID;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getHabitat() {
+		return habitat;
+	}
+
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+
+	public int getMinLeafSize() {
+		return minLeafSize;
+	}
+
+	public void setMinLeafSize(int minLeafSize) {
+		this.minLeafSize = minLeafSize;
+	}
+
+	public int getMaxLeafSize() {
+		return maxLeafSize;
+	}
+
+	public void setMaxLeafSize(int maxLeafSize) {
+		this.maxLeafSize = maxLeafSize;
+	}
+
+	public String getDistribution() {
+		return distribution;
+	}
+
+	public void setDistribution(String distribution) {
+		this.distribution = distribution;
+	}
+
+	public String getConservationStatus() {
+		return conservationStatus;
+	}
+
+	public void setConservationStatus(String conservationStatus) {
+		this.conservationStatus = conservationStatus;
+	}
+
+	public String getGrowthReq() {
+		return growthReq;
+	}
+
+	public void setGrowthReq(String growthReq) {
+		this.growthReq = growthReq;
+	}
+
+	public String getHorticulturalFeatures() {
+		return horticulturalFeatures;
+	}
+
+	public void setHorticulturalFeatures(String horticulturalFeatures) {
+		this.horticulturalFeatures = horticulturalFeatures;
+	}
+
+	public String getUses() {
+		return uses;
+	}
+
+	public void setUses(String uses) {
+		this.uses = uses;
+	}
+
+	public String getAssociatedFauna() {
+		return associatedFauna;
+	}
+
+	public void setAssociatedFauna(String associatedFauna) {
+		this.associatedFauna = associatedFauna;
+	}
+
+	public boolean isBookmarkStatus() {
+		return bookmarkStatus;
+	}
+
+	public void setBookmarkStatus(boolean bookmarkStatus) {
+		this.bookmarkStatus = bookmarkStatus;
+	}
+
+	public void setSciName(String sciName) {
+		this.sciName = sciName;
+	}
+
+	public void setComName(String comName) {
+		this.comName = comName;
+	}
+
+	public void setFamily(String family) {
+		this.family = family;
+	}
 
 } 
