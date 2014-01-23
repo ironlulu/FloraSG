6cbf3751e53a88d4fee7a7400bbd34ecf7ec9ba0
ironlulu <isabelzhoulu@yahoo.com>
2014-01-23 16:24:40 +0800

    News and NewsListRetriever

---
+++ floraSG/src/com/example/florasg/News.java
@@ -0,0 +1,66 @@
+package com.example.florasg;
+
+import java.net.URL;
+import java.util.Date;
+
+public class News {
+
+	int news_ID;
+	String title;
+	String content;
+	Date date;
+	URL url;
+	
+	public News(int news_ID, String title, String content, Date date, URL url) {
+		super();
+		this.news_ID = news_ID;
+		this.title = title;
+		this.content = content;
+		this.date = date;
+		this.url = url;
+	}
+
+	public int getNews_ID() {
+		return news_ID;
+	}
+
+	public void setNews_ID(int news_ID) {
+		this.news_ID = news_ID;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public URL getUrl() {
+		return url;
+	}
+
+	public void setUrl(URL url) {
+		this.url = url;
+	}
+	
+	
+	
+	
+}
---
+++ floraSG/src/com/example/florasg/NewsListRetriever.java
@@ -0,0 +1,18 @@
+package com.example.florasg;
+
+import java.util.List;
+
+public class NewsListRetriever {
+	List<News> newsList;
+	
+	public List<News> getLatestNews () {
+		
+		return newsList;
+		
+	}
+	
+	public News getNewsItem (int news_ID){
+		return null;
+		
+	}
+}
