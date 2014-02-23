eeaf1ccccfdcc006a8dde2c87fb7468f368be146
minwo324 <aprillee2412720@gmail.com>
2014-02-23 23:58:25 +0800

    Add bookmark toggle button on plantInfo page

--- floraSG/src/com/example/florasg/GUI/PlantInfo.java
+++ floraSG/src/com/example/florasg/GUI/PlantInfo.java
@@ -2,26 +2,33 @@ package com.example.florasg.GUI;
 
 import com.example.florasg.MainActivity;
 import com.example.florasg.R;
+import com.example.florasg.Controller.BookmarkManager;
 import com.example.florasg.Controller.PlantDataRetriever;
 import com.example.florasg.Model.Plant;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.support.v4.widget.DrawerLayout.LayoutParams;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PlantInfo extends Activity {
 	
 	private TableLayout infoTable;
 	private Plant plantObj;
 	PlantDataRetriever PDR;
+	BookmarkManager bm;
+	private String bookmarkStatus;
+	public boolean isBookmark;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +43,8 @@ public class PlantInfo extends Activity {
 			infoTable.addView(newImage);
 		}
 		*/
+		
+		// Open plantDatabase and get plantObj
 		PDR = new PlantDataRetriever(this);
 		PDR.openDB();
 		plantObj = PDR.getPlant(MainActivity.plant);
@@ -43,6 +52,19 @@ public class PlantInfo extends Activity {
 		TextView newTitle;
 		TextView newContent;
 		
+		// Open bookmarkManage and ready to add bookmark
+		bm = new BookmarkManager(this);
+		
+		// Check for bookmarkStatus
+		bookmarkStatus = plantObj.getBookmarkStatus();
+		
+		if (bookmarkStatus.equalsIgnoreCase("true")) {
+	        isBookmark = true;
+	    } else {
+	    	isBookmark = false;
+	    }
+		
+
 		// Display pictures
 		LinearLayout imgRow = (LinearLayout) findViewById(R.id.imageListScrollView);
 		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
@@ -186,6 +208,39 @@ public class PlantInfo extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.plant_info, menu);
 		return true;
+	}
+	
+	public boolean onPrepareOptionsMenu(Menu menu) {
+	    MenuItem fave = menu.findItem(R.id.add);
+	    MenuItem unfave = menu.findItem(R.id.remove);
+
+	    fave.setVisible(!isBookmark);
+	    unfave.setVisible(isBookmark);
+
+	    return true;
+	}
+	
+	@SuppressLint("NewApi")
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	    case R.id.add:
+	    	isBookmark = true;
+	        //bm.toggleBookmark(MainActivity.plant)
+	        invalidateOptionsMenu();
+	        Toast.makeText(getApplicationContext(), 
+                    "Add to Bookmark List!", Toast.LENGTH_LONG).show();
+	        //Toast("Removed from Favourites");
+	        return true;
+	    case R.id.remove:
+	        isBookmark = false;
+	        //bm.toggleBookmark(MainActivity.plant)
+	        invalidateOptionsMenu();
+	        Toast.makeText(getApplicationContext(), 
+                    "Deleted from Bookmark List!", Toast.LENGTH_LONG).show();
+	        return true;
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
 	}
 
 }
