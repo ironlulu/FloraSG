ababcb2425ad090c60d8730c83cc9ae89d164414
minwo324 <aprillee2412720@gmail.com>
2014-04-21 21:32:24 +0800

    Add AbouUs page

--- floraSG/src/com/example/florasg/AboutUsActivity.java
+++ floraSG/src/com/example/florasg/AboutUsActivity.java
@@ -1,9 +1,6 @@
 package com.example.florasg;
 
 import com.example.florasg.R;
-import com.example.florasg.R.layout;
-import com.example.florasg.R.menu;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
