7ce7a45e532628c82f0965125b92d148e35be2ba
minwo324 <aprillee2412720@gmail.com>
2014-04-24 00:15:52 +0800

    Modify the tabs design

--- floraSG/src/com/example/florasg/GUI/TabMainActivity.java
+++ floraSG/src/com/example/florasg/GUI/TabMainActivity.java
@@ -2,6 +2,7 @@ package com.example.florasg.GUI;
 
 import android.app.TabActivity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -22,47 +23,50 @@ import com.example.florasg.GUI.searchGUI.SearchActivity;
 @SuppressWarnings("deprecation")
 public class TabMainActivity extends TabActivity implements OnTabChangeListener {
 	/** Called when the activity is first created. */
+	
+	TabHost tabHost;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tab_main);
 
-		TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+		tabHost = (TabHost) findViewById(android.R.id.tabhost);
 
 		tabHost.setOnTabChangedListener(this);
 
 		/** search Tab */
 		TabSpec searchspec = tabHost.newTabSpec("Search");
 		// setting Title and Icon for the Tab
-		searchspec.setIndicator("", getResources().getDrawable(R.drawable.ic_tab_search));
+		searchspec.setIndicator("", getResources().getDrawable(R.drawable.ic_action_search));
 		Intent searchIntent = new Intent(this, SearchActivity.class);
 		searchspec.setContent(searchIntent);
 
 		/** browse Tab */
 		TabSpec browsespec = tabHost.newTabSpec("Browse");
 		// setting Title and Icon for the Tab
-		browsespec.setIndicator("", getResources().getDrawable(R.drawable.ic_tab_browse));
+		browsespec.setIndicator("", getResources().getDrawable(R.drawable.ic_action_browse));
 		Intent browseIntent = new Intent(this, BrowseActivity.class);
 		browsespec.setContent(browseIntent);
 
 		/** camera Tab */
 		TabSpec cameraspec = tabHost.newTabSpec("Camera");
 		// setting Title and Icon for the Tab
-		cameraspec.setIndicator("", getResources().getDrawable(R.drawable.ic_tab_camera));
+		cameraspec.setIndicator("", getResources().getDrawable(R.drawable.ic_action_camera));
 		Intent cameraIntent = new Intent(this, CameraActivity.class);
 		cameraspec.setContent(cameraIntent);
 
 		/** profile Tab */
 		TabSpec profilespec = tabHost.newTabSpec("Profile");
 		// setting Title and Icon for the Tab
-		profilespec.setIndicator("", getResources().getDrawable(R.drawable.ic_tab_profile));
+		profilespec.setIndicator("", getResources().getDrawable(R.drawable.ic_action_profile));
 		Intent profileIntent = new Intent(this, ProfileActivity.class);
 		profilespec.setContent(profileIntent);
 
 		/** news Tab */
 		TabSpec newsspec = tabHost.newTabSpec("News");
 		// setting Title and Icon for the Tab
-		newsspec.setIndicator("", getResources().getDrawable(R.drawable.ic_tab_news));
+		newsspec.setIndicator("", getResources().getDrawable(R.drawable.ic_action_news));
 		Intent newsIntent = new Intent(this, NewsActivity.class);
 		newsspec.setContent(newsIntent);
 
@@ -75,6 +79,13 @@ public class TabMainActivity extends TabActivity implements OnTabChangeListener 
 
 		// set Windows tab as default (zero based)
 		tabHost.setCurrentTab(0);
+		
+		// set background color of tabs
+		for(int i=0;i<tabHost.getTabWidget().getChildCount();i++)
+		{
+        	tabHost.getTabWidget().getChildAt(i).setBackgroundColor(Color.parseColor("#ffffff"));
+		}
+        tabHost.getTabWidget().getChildAt(0).setBackgroundColor(Color.parseColor("#cccbc1"));
 	}
 
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -115,6 +126,14 @@ public class TabMainActivity extends TabActivity implements OnTabChangeListener 
 		// TODO Auto-generated method stub
 		Log.i("Tab id", ""+tabId);
 		setTitle(tabId);
+		
+		// set the background color of tabs
+		for(int i=0;i<tabHost.getTabWidget().getChildCount();i++)
+		{
+			tabHost.getTabWidget().getChildAt(i).setBackgroundColor(Color.parseColor("#ffffff"));
+		}
+
+		tabHost.getTabWidget().getChildAt(tabHost.getCurrentTab()).setBackgroundColor(Color.parseColor("#cccbc1"));
 	}
 
 	@Override
